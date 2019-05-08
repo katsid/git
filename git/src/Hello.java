b0713a7cfcda64c3d61538b28568c58104b089f0
kat <katsid@163.com>
2019-05-08 18:11:14 +0800

    把本地工程添加到git

---
+++ git/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+    }
+}
